0b04a5ebf971e51544cadc694f606cd66177a0d0
collerhatguy <67057792+collerhatguy@users.noreply.github.com>
2021-08-31 11:03:16 -0400

    2 routes down

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,36 @@
 // implement your posts router here
+const express = require("express")
+const router = express.Router()
+const {
+    find,
+    findById,
+    insert,
+    update,
+    remove,
+    findPostComments,
+    findCommentById,
+    insertComment,
+} = require("./posts-model");
+const err = { message: "whoops" }
+
+router.get("/api/posts", (req, res) => {
+    find().then(posts => {
+        res.status(200).json(posts)
+    }).catch(() => {
+        res.status(500).json(err)
+    })
+})
+
+router.get("/api/posts/:id", (req, res) => {
+    const { id } = req.params;
+    if (!id) return res.status(400).json(err)
+    findById(id).then(post => {
+        post ?
+            res.status(200).json(post)
+            :
+            res.status(404).json(post)
+
+    }).catch(() => {
+        res.status(500).json(err)
+    })
+})
